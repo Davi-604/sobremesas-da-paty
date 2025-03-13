d30a62ff6ac430444bf74a185fedf5c7d3fc965e
Davi-604 <davimedeiroscv100@gmail.com>
2025-03-13 15:09:32 -0300

    optimazing the form that the metadata functions are called

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,43 @@ import './globals.css';
 import { ThemeProvider } from '@/components/themes/ThemeProvider';
 import { Header } from '@/components/navigation/Header';
 import { Footer } from '@/components/default/Footer';
+import { getWebsiteData } from '@/services/website';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
-    title: 'Sobremesas da Paty | Doces e sobremesas para a cidade de Patos de Minas',
-};
+export async function generateMetadata(): Promise<Metadata> {
+    const metadata = await getWebsiteData();
+
+    return {
+        title: metadata.title,
+        description: metadata.description,
+        icons: {
+            icon: metadata.icon,
+            shortcut: metadata.icon,
+            apple: metadata.icon,
+        },
+        openGraph: {
+            title: metadata.title,
+            description: metadata.description,
+            url: 'https://www.sobremesasdapaty.com/',
+            images: [{ url: metadata.logo, width: 1200, height: 630 }],
+        },
+        twitter: {
+            card: 'summary_large_image',
+            title: metadata.title,
+            description: metadata.description,
+            images: [metadata.logo],
+        },
+        keywords: [
+            'bolos',
+            'doces',
+            'confeitaria',
+            'sobremesas',
+            'doçaria',
+            'doces caseiros',
+        ],
+    };
+}
 
 export default function RootLayout({
     children,
